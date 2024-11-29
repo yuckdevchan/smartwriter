a53c12c019eb252db9adfa3587adfc23a6a2c07c
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2024-11-29 18:21:26 +0000

    test

--- editor.js
+++ editor.js
@@ -26,3 +26,40 @@ function contextPaste() {
 
 function contextPasteWithoutFormatting() {
 }
+
+function closeGeneratePopup() {
+    const generatePopup = document.querySelector('#generatePopup');
+    generatePopup.style.opacity = 0;
+    generatePopup.style.pointerEvents = 'none';
+    document.querySelector(".main-content").removeEventListener('click', closeGeneratePopup);
+}
+
+function openGeneratePopup(e) {
+    closeQuillContextMenu();
+    const generatePopup = document.querySelector('#generatePopup');
+    generatePopup.style.top = `${e.clientY}px`;
+    generatePopup.style.left = `${e.clientX}px`;
+    generatePopup.style.opacity = 1;
+    generatePopup.style.pointerEvents = 'auto';
+    document.querySelector(".main-content").addEventListener('click', closeGeneratePopup);
+}
+
+document.querySelector("#openGeneratePopupButton").addEventListener('click', openGeneratePopup);
+
+function generateText() {
+    const generateInput = document.querySelector('#generateInput');
+    const prompt = encodeURIComponent(generateInput.value);
+    fetch(`http://localhost:5000/api/promptGemini`, {
+        method: 'POST',
+        body: JSON.stringify({ prompt: prompt }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        quill.pasteHTML(data.text);
+        closeGeneratePopup();
+    })
+    .catch(error => console.error('Error:', error));
+}
